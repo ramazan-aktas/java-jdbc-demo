b9c3ef9679ca9173fa14ced5c28030dc1588d573
ramazan-aktas <111694852+ramazan-aktas@users.noreply.github.com>
2022-08-23 21:32:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,21 +5,7 @@ import java.sql.Statement;
 public class Main {
 
 	public static void main(String[] args) {
-		Connection con = (new Person()).connectTo("root", "malatya44");
-		try {
-			Statement st = con.createStatement();
-			//st.execute("delete from people");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Person ramazan = new Person(1, "Ramazan Aktaş", 30);
-		Person feyzi = new Person(2, "Muhammet Feyzi Sağlam", 31);
-		Person lina = new Person(3, "Lina Sağlam", 1);
-		ramazan.DBCreate(con);
-		ramazan.DBUpdate(con, lina);
-		feyzi.DBDelete(con);
+		Connection con = (new Person()).connectTo("root", "password");
 	}
 
 }
